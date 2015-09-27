57146a0ee76a1bdaf0cbbe6bc6acb39e4d3875ba
Nicolai Froehlich <launchpad@nifr.de>
2015-09-27 14:05:09 +0200

    Fixed handlers. Refactored tasks.

---
+++ tests/setup/specs/composer_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../../config/serverspec/spec_helper"
+# File.expand_path('relative/path', File.dirname(FILE))
+
+describe file('/opt/composer/composer.phar') do
+  it { should be_file }
+  it { should be_mode 755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/bin/composer') do
+  it { should be_linked_to '/opt/composer/composer.phar' }
+end
+
+
